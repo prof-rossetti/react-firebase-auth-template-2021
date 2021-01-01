e8f4205f005b7d0416ff8c4830a07a4448131ba1
MJ Rossetti <s2t2@users.noreply.github.com>
2021-01-01 11:02:37 -0500

    Show example flash messages

--- src/components/FlashContainer.js
+++ src/components/FlashContainer.js
@@ -4,11 +4,17 @@ import React from 'react'
 
 //import { useFlash, FlashProvider } from "../contexts/Flash"
 
+import AlertDismissable from "./AlertDismissable"
+
+const flashes = [
+    {message:"Hello", variant:"info"},
+    {message:"Goodbye", variant:"dark"}
+]
+
 export default function FlashContainer() {
     //const { currentFlash } = useFlash()
 
-    return (
-
-        <p>CURRENT FLASH: </p>
-    )
+    return flashes.map(function({message, variant}){
+        return <AlertDismissable key={message} message={message} variant={variant} />
+    })
 }
