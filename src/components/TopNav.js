280413a3ae317ec432d8820d062eb325261cacb3
MJ Rossetti <s2t2@users.noreply.github.com>
2021-01-06 17:54:21 -0500

    WIP there is an issue with the ability to reliably detect the expanded state of the nav

--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -1,12 +1,19 @@
 
-import React from 'react'
+import React, {useState} from 'react'
 import {Navbar, Nav, Image} from 'react-bootstrap'
-//import {GearIcon} from '@primer/octicons-react'
 
 import { useAuth } from "../contexts/FirebaseAuth"
 
+// https://github.com/react-bootstrap/react-bootstrap/issues/5597
+
 export default function TopNav() {
     const { currentUser } = useAuth()
+    const [expanded, setExpanded] = useState(false)
+
+    function overrideToggle() {
+        console.log("TOGGLE TRIGGERED...")
+        setExpanded(prevExpanded => !prevExpanded)
+    }
 
     return (
         <Navbar bg="light" // controls nav background color
@@ -15,6 +22,11 @@ export default function TopNav() {
             expand="sm" // collapse when less wide than this breakpoint
             collapseOnSelect // collapse after a nav link has been clicked
             style={{marginBottom:"15px"}}
+
+            // OVERRIDING DEFAULT BEHAVIOR SO WE CAN DETECT CURRENT TOGGLED STATE
+            expanded={expanded} // only need to add this when configuring the nav to detect its expanded state
+            onToggle={overrideToggle}
+
         >
             <Navbar.Brand href="/" style={{fontSize:22}}>My Site</Navbar.Brand>
 
@@ -26,12 +38,10 @@ export default function TopNav() {
                     {!currentUser &&
                         <Nav.Link href="/login">Login</Nav.Link>
                     }
-
-
                 </Nav>
 
                 { currentUser &&
-                    <Nav.Link href="/profile">
+                    <Nav.Link href="/profile" style={ expanded ? {paddingLeft:"0px"} : {paddingLeft: "1rem"}}>
                         <Image roundedCircle
                             src={currentUser.photoURL}
                             alt="user profile"
