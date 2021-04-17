40cea1e654ab0d47a9b89e05b1fac2deb64933a0
MJ Rossetti <s2t2@users.noreply.github.com>
2021-04-17 11:47:03 -0400

    Capture order info

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,32 +1,48 @@
 
 
 import React, {PureComponent} from 'react'
+import { useHistory } from 'react-router-dom'
 import ReactGA from 'react-ga'
 import { Container, Row, Col, Card, Button } from 'react-bootstrap'
 
 import {fetchProducts} from "../firebase"
+import { useAuth } from "../contexts/FirebaseAuth"
+import { useFlashUpdate } from "../contexts/FlashContext"
 
 function ProductCard(props) {
-    const title = props.title || props.name || "Product XYZ"
-    const description = props.description || "An amazing product anyone would love to buy!"
-    const imgSrc = props.imgSrc || props.image_url || "https://picsum.photos/180/100"
+    const product = props.product
+    //const title = props.title || "Product XYZ"
+    //const description = props.description || "An amazing product anyone would love to buy!"
+    //const imgSrc = props.imgSrc || "https://picsum.photos/180/100"
 
-    function handleClick(event){
-        console.log("YOU CLICKED PRODUCT:", title)
-        ReactGA.event({category: "Product", action: "Click", label: title})
-        // and probably do something else here ...
+    const { currentUser } = useAuth()
+    const flash = useFlashUpdate()
+    const history = useHistory()
+
+    function handleClick(){
+        console.log("YOU CLICKED PRODUCT:", product.name)
+        ReactGA.event({category: "Product", action: "Click", label: product.name})
+
+        if(currentUser){
+            console.log("----------------------")
+            console.log("SENDING THE ORDER...")
+            console.log("USER:", currentUser.uid, currentUser.email)
+            console.log("PRODUCT:", product.id, product.name)
 
 
-
+        } else {
+            flash({message:"Oh, to order you must be logged in!", variant: "warning"})
+            history.push("/login")
+        }
     }
 
     return (
         <Card style={{ marginBottom: '20px' }}>
-            <Card.Img variant="top" src={imgSrc} />
+            <Card.Img variant="top" src={product.image_url} />
             <Card.Body>
-                <Card.Title>{title}</Card.Title>
-                <Card.Text>{description}</Card.Text>
-                <Button variant="primary" onClick={handleClick}>Go somewhere</Button>
+                <Card.Title>{product.name}</Card.Title>
+                <Card.Text>{product.description}</Card.Text>
+                <Button variant="primary" onClick={handleClick}>Order now!</Button>
             </Card.Body>
         </Card>
     );
@@ -42,11 +58,7 @@ export default class Products extends PureComponent {
         var cols = this.state.products.map((product) => {
             return (
                 <Col key={product.id}>
-                    <ProductCard key={product.id}
-                        title={product.name}
-                        description={product.description}
-                        imgSrc={product.image_url}
-                    />
+                    <ProductCard key={product.id} product={product}/>
                 </Col>
             )
         })
