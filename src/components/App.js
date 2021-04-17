0a36b6cede7c8d15aa1d9e1c4540da3fd26a8783
MJ Rossetti <s2t2@users.noreply.github.com>
2021-04-17 12:47:21 -0400

    Commenting

--- src/components/App.js
+++ src/components/App.js
@@ -9,23 +9,23 @@ import { AuthProvider } from "../contexts/FirebaseAuth"
 import { FlashProvider } from "../contexts/FlashContext"
 import ProtectedRoute from "./ProtectedRoute"
 
+import FlashContainer from "./FlashContainer"
 import TopNav from "./TopNav"
 //import Footer from "./Footer"
+
 import Home from "./Home"
 import Products from "./Products"
 import About from "./About"
 import Login from "./GoogleLogin"
-import Profile from "./Profile"
-import FlashContainer from "./FlashContainer"
-
-import AlertMe from "./AlertMe"
-import FlashForm from "./FlashForm"
+import UserProfile from "./Profile"
+import AlertMe from "./AlertMe" // example page for seeing different alert styling options
+import FlashForm from "./FlashForm" // example page for testing and debugging the flash
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-ReactGA.initialize(process.env.REACT_APP_GA_TRACKER_ID, {
-    debug: (process.env.REACT_APP_GA_DEBUG_MODE === "true")
-})
+const GA_TRACKER_ID = process.env.REACT_APP_GA_TRACKER_ID
+const GA_DEBUG_MODE = (process.env.REACT_APP_GA_DEBUG_MODE === "true")
+ReactGA.initialize(GA_TRACKER_ID, {debug: GA_DEBUG_MODE})
 
 export default function App() {
     ReactGA.pageview(window.location.href)
@@ -45,7 +45,7 @@ export default function App() {
                             <Route exact path="/about" component={About} />
                             <Route exact path="/products" component={Products} />
                             <Route exact path="/login" component={Login} />
-                            <ProtectedRoute exact path="/profile" component={Profile} />
+                            <ProtectedRoute exact path="/profile" component={UserProfile} />
 
                             <Route path="/alerts" component={AlertMe} />
                             <Route path="/flashes" component={FlashForm} />
